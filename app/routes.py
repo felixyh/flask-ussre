b22bab94b80202d930079a96f2e32efa3cd479c9
felixyh <felixyanghui@gmail.com>
2024-01-14 12:46:03 -0600

    chapter-2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello, world!"
+    # return "hello, world!"
+    user = {'username': 'Felix_Yanghui'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Too cold today in Dallas!'
+        },
+        {
+            'author': {'username': 'Tony'},
+            'body': 'SKO will be hold tomorrow in Olando!'
+
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
